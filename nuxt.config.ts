5d050f93a11a2cc4afcf0eecd93a646980bdf639
gonzalo.moreno <gonzalo.moreno@edreamsodigeo.com>
2026-06-26 01:24:45 +0100

    Add deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const categories = ['absoluto', 'junior', 'infantil', 'alevin']
+const modalities = ['duo', 'duo-mixto', 'combo', 'equipo']
+const competitionRoutes = categories.flatMap(c => modalities.map(m => `/${c}/${m}`))
+const adminRoutes = categories.flatMap(c => modalities.map(m => `/admin/${c}/${m}`))
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -19,6 +25,11 @@ export default defineNuxtConfig({
       firebaseStorageBucket: '',
       firebaseMessagingSenderId: '',
       firebaseAppId: '',
+    },
+  },
+  nitro: {
+    prerender: {
+      routes: ['/', '/login', '/admin', ...competitionRoutes, ...adminRoutes],
     },
   },
 })
